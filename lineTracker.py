dc2e663df9b6bb20a8a11b5de291af0ada4f3dc7
James Vaughn <jvaughn0822@gmail.com>
2017-08-12 09:53:43 -0700

    Add files via upload

--- lineTracker.py
+++ lineTracker.py
@@ -4,26 +4,22 @@ from collections import deque
 # Define a class to receive the characteristics of each line detection
 class Line():
     def __init__(self):
-        # was the line detected in the last iteration?
-        self.detected = False
-        # x values of the last n fits of the line
-        self.recent_xfitted = deque([], 6)
-        #average x values of the fitted line over the last n iterations
-        self.bestx = None
-        #polynomial coefficients averaged over the last n iterations
-        #self.best_fit = None
-        #polynomial coefficients for the most recent fit
-        self.current_fit = [np.array([False])]
-        #radius of curvature of the line in some units
-        self.radius_of_curvature = None
-        #distance in meters of vehicle center from the line
-        self.line_base_pos = None
-        #difference in fit coefficients between last and new fits
-        #self.diffs = np.array([0,0,0], dtype='float')
-        #x values for detected line pixels
-        self.allx = None
-        #y values for detected line pixels
-        self.ally = None
+        self.detected = False         # was the line detected in the last iteration?
+        
+        self.recent_xfitted = deque([], 6)        # x values of the last n fits of the line
+        
+        self.bestx = None        #average x values of the fitted line over the last n iterations
+        
+        self.current_fit = [np.array([False])]        #polynomial coefficients for the most recent fit
+
+        self.radius_of_curvature = None        #radius of curvature of the line in some units
+
+        self.line_base_pos = None        #distance in meters of vehicle center from the line
+
+        self.allx = None        #x values for detected line pixels
+
+        self.ally = None        #y values for detected line pixels
+
 
     def update_line(self, x, y, fitparam, xfit, curv, dist):
         self.detected = True
